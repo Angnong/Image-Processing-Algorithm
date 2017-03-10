4df4c64e92241fd2d5f6cb25eb378502737b5276
Angnong <angnong.zhang@gmail.com>
2017-03-10 16:15:14 +0100

    Add files via upload

--- Segmentation/region_growing.m
+++ Segmentation/region_growing.m
@@ -5,21 +5,25 @@ function S = region_growing(I, x_start, y_start, threshold)
 S = zeros(m,n);
 p_start = sub2ind(size(I),x_start,y_start);
 S(p_start) = 1;
-ap_list = [p_start-m;p_start-1;p_start+1;p_start+m];
+ap_list = [p_start-m;p_start-1;p_start+1;p_start+m]; % 四邻域拓展
 region_mean = I(x_start,y_start);
 count = 1;
     while ~isempty(ap_list)
       x = find(abs(I(ap_list)-region_mean)==min(abs(I(ap_list)-region_mean)));
       p = x;
-        if abs(I(ap_list(p(1)))-region_mean) > threshold
+      if abs(I(ap_list(p(1)))-region_mean) > threshold
         return
-        end 
-      S(ap_list(p)) = 1;
+      end
+
+      S(ap_list(p)) = 1; % treate it as region element
       pixel = I(ap_list(p(1)));
+      % 弹出元素并且压入当前区域的边界位置元素
       ap_list(p) = [];
-      N = conv2(double(S),[0,1,0;1,0,1;0,1,0],'same');
-      ap_list = find(N > 0 & S < 1);
+      N = conv2(double(S),[0,1,0;1,0,1;0,1,0],'same'); 
+      ap_list = find(N > 0 & S < 1); % find the current region borders
+      
       count = count+length(p);
+      % update region mean
       region_mean = (region_mean*(count-length(p))+pixel*length(p))/count;
       
     end
--- Segmentation/region_test.m
+++ Segmentation/region_test.m
@@ -1,5 +1,8 @@
-%% Region Growing
+%% 区域生长算法
+%% Parameter
+threshold = 0.15
 
+%% function
 %read image
 %I = im2double(imread('./images/coins.png'));
 I = im2double(imread('./images/lena.png'));
@@ -12,7 +15,7 @@ imshow(I);
 [y,x] = getpts;
 x_start = round(x);
 y_start = round(y);
-S = region_growing(I,x_start,y_start,0.15);
+S = region_growing(I,x_start,y_start,threshold);
 
 imshow(S);
 imwrite(S,'./images/segment_lena.png');
