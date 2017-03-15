1623471f92d3e86ea7b3c035963963d4af5fc421
Angnong <angnong.zhang@gmail.com>
2017-03-15 14:28:20 +0100

    Add files via upload

--- Segmentation/region_growing.m
+++ Segmentation/region_growing.m
@@ -1,6 +1,14 @@
 function S = region_growing(I, x_start, y_start, threshold)
-% input: Image I, position(x_start,ystart), threshold
-% output: segmented region
+%% 算法描述
+%{
+  输入图像，所分割区域初始点及阀值
+  1. 将初始点的四邻域放到搜索列表，设初始点的值为区域均值
+  2. 找到搜索列表中与均值差值最小的点
+  3. 判断最小差值是否已超过阀值，若是，则结束，若不是，则将此点加入区域集并从搜索列表剔除
+  4，寻找当前区域集的边界，并将其加入搜索列表，并更新区域均值
+  5. 重复2-4直到搜索列表为空，返回分割后的区域
+}%
+
 [m,n] = size(I);
 S = zeros(m,n);
 p_start = sub2ind(size(I),x_start,y_start);
